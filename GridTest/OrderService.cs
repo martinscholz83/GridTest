0937fba47bd2c1400b802d087bbd0a6d5378c0fb
Martin Scholz <martin.scholz@versasec.com>
2023-03-07 09:19:39 +0100

    First commit

---
+++ GridTest/OrderService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using GridCore.Server;
+using GridShared;
+using GridShared.Utility;
+using Microsoft.Extensions.Primitives;
+
+namespace GridTest
+{
+    public class Order
+    {
+        [Key]
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public string Street { get; set; }
+    }
+
+    public class OrderService
+    {
+        public ItemsDTO<Order> GetOrdersGridRows(Action<IGridColumnCollection<Order>> columns,
+                QueryDictionary<StringValues> query)
+        {
+            var items = new List<Order>() {
+                new Order() {CustomerID = "1", OrderID = 1, Street = "Test"},
+                new Order() {CustomerID = "2", OrderID = 2, Street = "Test"},
+                new Order() {CustomerID = "3", OrderID = 3, Street = "Test"}
+            };
+            var server = new GridCoreServer<Order>(items, query,
+                true, "ordersGrid", columns, 10).Sortable().Filterable();
+
+            // return items to displays
+            return server.ItemsToDisplay;
+        }
+    }
+}
+
---
+++ GridTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using GridTest.Data;
+using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
+using static GridTest.Pages.Index;
+using GridTest;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<OrderService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
+
